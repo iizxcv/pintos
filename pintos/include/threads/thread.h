5487a342fd194f46d1456d864f3a1fb51916edb0
iizxcv <jmlee9471@gmail.com>
2025-07-27 22:05:14 +0900

    일단 커밋 wait()구현중

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -5,6 +5,7 @@
 #include <list.h>
 #include <stdint.h>
 #include "threads/interrupt.h"
+#include "threads/synch.h"
 #ifdef VM
 #include "vm/vm.h"
 #endif
@@ -100,6 +101,11 @@ struct thread {
 	/* Owned by userprog/process.c. */
 	uint64_t *pml4;                     /* Page map level 4 */
 	struct fd_table *fd_table;
+
+	struct list_elem* process_parent_elem;
+	struct list_elem process_cur_elem;
+	struct list process_child_list;
+	struct semaphore goreajang;
 #endif
 #ifdef VM
 	/* Table for whole virtual memory owned by thread. */
